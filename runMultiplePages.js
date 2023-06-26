784f997b85489394895172c28a87b2b92483288b
Eric Ordonez <jwustudent@gmail.com>
2023-06-26 16:39:25 -0500

    changed the code thats executed in cmd line

--- runMultiplePages.js
+++ runMultiplePages.js
@@ -10,7 +10,7 @@ let runs = 0
 for (const url of urls) {
     console.log(`Running performance test ${runs + 1}`); // Logs this to the console just before it kicks off
     try {
-        execSync(`lighthouse ${url}`); // Executes this on the command line to run the performance test
+        execSync(`lighthouse ${url} --view`); // Executes this on the command line to run the performance test
     }
     catch(err) {
         console.log(`Performance test ${runs + 1} failed`); // If Lighthouse happens to fail it'll log this to the console and log the error message
