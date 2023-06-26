b369f6684f494fc4c7f4ac4a4d44b52c8c3e433f
Eric Ordonez <jwustudent@gmail.com>
2023-06-26 16:43:40 -0500

    Changed the executable cmd to add view for reports

--- runLighthouseDecorPlanet.js
+++ runLighthouseDecorPlanet.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 // This script will run performance and accessibility tests on whatever url is entered. 
 //Edit the runlimit to adjust the amount of iterations performed
+//The default runLimit is set to 2. Adjust based on preferences
 
 
 const execSync = require('child_process').execSync;
-let url = "https://www.decorplanet.com/"; // Url that runs the tests
+let url = "https://www.reggioregister.com/"; // Url that runs the tests
 let runs = 0;
-let runLimit = 1; // the number of performance tests to run
+let runLimit = 2; // the number of performance tests to run
 do {
     console.log(`Starting performance test ${runs + 1}`); // Logs this to the console just before it kicks off
     try {
-        execSync(`lighthouse ${url}`); // Executes this on the command line to run the performance test
+        execSync(`lighthouse ${url} --view`); // Executes this on the command line to run the performance test
     }
     catch(err) {
         console.log(`Performance test ${runs + 1} failed`); // If Lighthouse fails display this error message in console
